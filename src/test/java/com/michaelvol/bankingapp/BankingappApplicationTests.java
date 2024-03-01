5db27beb958d580dcc570d8557be28d3e12ac585
volakism <michael@Inter>
2024-03-01 11:44:11 +0200

    feat: initial commit

---
+++ src/test/java/com/michaelvol/bankingapp/BankingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelvol.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
