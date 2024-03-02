fc78d3f58eb798ca4822a9941f0c7d1c842b9bf4
Michael Volakis <mivolakis@gmail.com>
2024-03-02 23:05:13 +0200

    feat: add account entity

---
+++ src/main/java/com/michaelvol/bankingapp/Account/Entity/Account.java
@@ -0,0 +1,47 @@
+package com.michaelvol.bankingapp.Account.Entity;
+
+import com.michaelvol.bankingapp.Account.enums.AccountStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Currency;
+
+/**
+ * Represents the Account entity that stores the user's bank accounts
+ */
+@Entity
+@Table(name = "accounts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Account{
+    @Id
+    @SequenceGenerator(name = "accounts_gen", sequenceName = "accounts_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "accounts_gen")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "balance", nullable = false)
+    private BigDecimal balance;
+
+    @Column(name = "currency", nullable = false)
+    private Currency currency;
+
+    @Enumerated
+    private AccountStatus status;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
---
+++ src/main/java/com/michaelvol/bankingapp/Account/enums/AccountStatus.java
@@ -0,0 +1,7 @@
+package com.michaelvol.bankingapp.Account.enums;
+
+public enum AccountStatus {
+    ACTIVE,
+    INACTIVE,
+    CLOSED
+}
