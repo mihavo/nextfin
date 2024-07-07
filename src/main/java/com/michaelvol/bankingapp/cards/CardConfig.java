022f14a5ab9a216c0aabe75357b7dc70a3eaee45
Michael Volakis <mivolakis@gmail.com>
2024-07-07 16:00:39 +0300

    add datafaker config

---
+++ src/main/java/com/michaelvol/bankingapp/cards/CardConfig.java
@@ -0,0 +1,17 @@
+package com.michaelvol.bankingapp.cards;
+
+import net.datafaker.Faker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.util.Locale;
+
+@Configuration
+public class CardConfig {
+
+    @Bean
+    public Faker FakerConfig() {
+        return new Faker(new Locale(LocaleContextHolder.getLocale().toLanguageTag()));
+    }
+}
--- src/main/java/com/michaelvol/bankingapp/cards/service/impl/CardNumberServiceImpl.java
+++ src/main/java/com/michaelvol/bankingapp/cards/service/impl/CardNumberServiceImpl.java
@@ -2,11 +2,15 @@ package com.michaelvol.bankingapp.cards.service.impl;
 
 import com.michaelvol.bankingapp.cards.service.def.CardNumberService;
 import lombok.RequiredArgsConstructor;
+import net.datafaker.Faker;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CardNumberServiceImpl implements CardNumberService {
+
+    private final Faker faker;
+
 
     @Override
     public String generate() {
