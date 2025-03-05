894681097fad2143a137c0cc447b53a40510fe2b
Michael Volakis <mivolakis@gmail.com>
2025-03-05 18:44:52 +0200

    feat: add caching deps, add redis config, add health check

---
+++ src/main/java/com/nextfin/config/cache/RedisConfig.java
@@ -0,0 +1,67 @@
+package com.nextfin.config.cache;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private JedisConnectionFactory connFactory;
+
+    @Value("{nextfin.cache.host}")
+    private String host;
+
+    @Value("{nextfin.cache.port}")
+    private int port;
+
+    @Value("{nextfin.cache.password}")
+    private String password;
+
+    @Value("{nextfin.cache.database}")
+    private int database;
+
+    @Value("{nextfin.cache.allowed:true}")
+    private boolean isCachingAllowed;
+
+    private static final AtomicBoolean isCachingEnabled = new AtomicBoolean(false);
+
+    @Bean
+    public RedisConnectionFactory connectionFactory() {
+        if (!isCachingAllowed || ObjectUtils.anyNull(host, port, password, database)) {
+            return null;
+        }
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+        redisConfig.setDatabase(database);
+        redisConfig.setPassword(password);
+        return new JedisConnectionFactory(redisConfig);
+    }
+
+    @PostConstruct
+    public void evaluateConnection() {
+        try (RedisConnection connection = connFactory.getConnection()) {
+            String ping = connection.ping();
+            if (ping != null && ping.equals("PONG")) {
+                log.info("Connection with Cache client established: {}", connection.getClientName());
+                isCachingEnabled.set(true);
+                return;
+            }
+            throw new RuntimeException("Connection could not be established: unexpected response");
+        } catch (Exception e) {
+            log.error("Connection with Cache Client failed: {}", e.getMessage());
+        }
+    }
+
+}
